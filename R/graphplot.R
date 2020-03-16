79da192845b6a0c95b3382b8b269dd046bc1a3f2
Simon De Deyne <simon2d@gmail.com>
2020-03-16 20:51:35 +1100

    Add graphplot

---
+++ R/graphplot.R
@@ -0,0 +1,27 @@
+#' Make a random plot
+#' 
+#' This function creates a random histogram plot.
+#' 
+#' @export
+#' @param n numer of random values 
+#' @param dist one of "normal" or "uniform".
+randomplot <- function(n, dist=c("normal", "uniform")){
+  library(ggplot2)
+  #input validation
+  dist <- match.arg(dist)
+  stopifnot(n < 1e6)
+  
+  if(dist == "normal"){
+    #hist(rnorm(n))
+  }
+  
+  if(dist == "uniform"){
+    #hist(runif(n))
+  }
+  
+  X = data.frame(x = runif(10),y = runif(10))
+  ggplot(X,aes(x=x,y=y)) + geom_point()
+  
+  #return nothing
+  invisible();  
+}
